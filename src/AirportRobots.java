3157f4e5bbd69d9aaca50ac27f453e9b2afdeb8c
ToughJ <38240717+ToughJ@users.noreply.github.com>
2018-10-22 14:42:58 +0800

    Add files via upload

--- src/AirportRobots.java
+++ src/AirportRobots.java
@@ -13,20 +13,20 @@ public class AirportRobots {
 
 	private static void doModel() throws IloException {
 		IloCplex model = new IloCplex();
-		IloNumVar[] x = new IloNumVar[3];
-		IloNumVar[] y = new IloNumVar[2];
-		for (int i = 0; i < 3; i++)
+		IloNumVar[] x = new IloNumVar[4];
+		IloNumVar[] y = new IloNumVar[4];
+		for (int i = 0; i < 4; i++)
 			x[i] = model.intVar(0, 34,"x"+i);
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < 4; i++)
 			y[i] = model.intVar(0, 34,"y"+i);
-		model.addGe(model.sum(x[0],y[0]), 34);
-		model.addGe(model.sum(model.sum(x[0], y[0]), model.sum(x[1],y[1])), 34);
-		model.addGe(model.sum(model.sum(x[1], y[0]), model.sum(x[2],y[1])), 17);
-		model.addGe(model.sum(x[1],y[0]), 17);
+		model.addGe(model.sum(x[0],x[3],y[0],y[2],y[3]), 34);
+		model.addGe(model.sum(x[0],y[0],x[1],y[1],y[3]), 34);
+		model.addGe(model.sum(x[1],y[0],x[2],y[1],y[2]), 17);
+		model.addGe(model.sum(x[2],x[3],y[1],y[2],y[3]), 17);
 		IloLinearNumExpr e = model.linearNumExpr();;
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < 4; i++)
 			e.addTerm(120, x[i]);
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < 4; i++)
 			e.addTerm(252, y[i]);
 		model.addMinimize(e);
 		model.exportModel("AirportRobots.lp");
